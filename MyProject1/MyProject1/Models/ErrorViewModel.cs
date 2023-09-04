956f731e8caefbf797fa59116a6b926b24fcb456
louisivannn <126262734+louisivannn@users.noreply.github.com>
2023-09-05 00:24:40 +0800

    Delete MyProject1 directory

--- MyProject1/MyProject1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MyProject1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
